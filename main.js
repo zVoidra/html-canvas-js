fced7c1c3e79677bb49547e72ef922776590b8ec
Name <zVoidra@outlook.com>
2024-11-16 22:31:51 +0100

    Added basic routing and other improvements.

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./index.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function loadContent(path) {
+  const relativePath = "/source/pages/" + path + "/" + path + ".html";
+  try {
+    const view = await fetch(relativePath).then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to load ${relativePath}: ${res.statusText}");
+      }
+      return res.text();
+    });
+    console.log(relativePath);
+    document.getElementById("app").innerHTML = view;
+  } catch (error) {
+    console.error(error);
+    document.getElementById("app").innerHTML = "<h1>Page not found.</h1>";
+  }
+}
 
-setupCounter(document.querySelector('#counter'))
+function router() {
+  const routes = {
+    "/": "home",
+    "/sol": "sol",
+  };
+
+  const path = window.location.pathname;
+  const route = routes[path] || "home";
+  loadContent(route);
+}
+
+function navigate(path) {
+  window.history.pushState({}, "", path);
+  router();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  router();
+
+  document.querySelectorAll("a[data-link]").forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const href = e.target.getAttribute("href");
+      navigate(href);
+    });
+  });
+});
+
+window.addEventListener("popstate", router);
