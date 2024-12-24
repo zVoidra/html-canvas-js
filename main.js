f7e095ebca34455f8c520965ca0ff767939decfb
Name <zVoidra@outlook.com>
2024-12-24 19:14:26 +0300

    Added orbital trails.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as SolPage from "./src/pages/sol/sol.js";
 import * as ClockPage from "./src/pages/clock/clock.js";
 import * as PanoramaPage from "./src/pages/panorama/panorama.js";
 import * as LinesPage from "./src/pages/lines/lines.js";
+import * as OrbitalTrails from "./src/pages/lines/lines.js";
 
 const routes = {
   "/": HomePage,
